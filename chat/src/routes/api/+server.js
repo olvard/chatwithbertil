37c3acfb0e2878bea31b90410b039f3cad5c250d
Oliver Lundin <olilu316@student.liu.se>
2023-10-12 13:22:15 +0200

    dynamic env

--- chat/src/routes/api/+server.js
+++ chat/src/routes/api/+server.js
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit'
-import { OPENAI_API_KEY } from '$env/static/private'
+import { OPENAI_API_KEY } from '$env/dynamic/private'
 
 export const POST = async (event) => {
   const requestBody = await event.request.json();
